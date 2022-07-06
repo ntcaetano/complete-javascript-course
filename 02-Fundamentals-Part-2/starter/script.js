586329e93452d9a403f065d4c94b12c3f6629005
Natanael Caetano <natacaet@gmail.com>
2022-07-06 17:21:24 -0300

    up

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,110 @@
+'use strict';
+
+// function logger() {
+//   console.log('My name is Jonas');
+// }
+
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const applejuice = fruitProcessor(5, 0);
+// console.log(applejuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeuice);
+
+// //Function declaration
+// const age1 = calAge1(1991);
+
+// function calAge1(birthYeah) {
+//   return 2037 - birthYeah;
+// }
+
+
+// //Function expression
+// const calAge2 = function(birthYeah) {
+//   return 2037 - birthYeah;
+// }
+// const age2 = calAge2(1991);
+
+// console.log(age1, age2);
+
+// Arrow Function
+// const calAge3 = birthYeah => 2037 - birthYeah;
+// const age3 = calAge3(1991);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYeah, firstName) => {
+//     const age = 2037 - birthYeah;
+//     const retirement = 65 - age;
+//     //   return retirement;
+//     return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1980, 'Bob'));
+
+// function cutFruitPieces(fruit) {
+//     return fruit * 4;
+//   }
+  
+//   function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+  
+//     const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
+//     return juice;
+//   }
+//   console.log(fruitProcessor(2, 3));
+
+//  function cortarFrutas(fruta) {
+//     return fruta * 4;
+//  }
+//     function processarFruta(maca, laranja){
+//         const pedacosMaca = cortarFrutas(maca);
+//         const pedacosLaranja = cortarFrutas(laranja);
+
+//         const suco = `Suco com ${pedacosMaca} pedaços de maça e ${pedacosLaranja} pedaços de laranja.`;
+//         return suco;
+//     }
+
+// console.log(processarFruta(2, 3))
+
+// const calcAge = function (birthYeah) {
+//     return 2037 - birthYeah;
+//   }
+  
+//   const yearsUntilRetirement = function (birthYeah, firstName) {
+//     const age = calcAge(birthYeah);
+//     const retirement = 65 - age;
+  
+//     if (retirement > 0) {
+//       console.log(`${firstName} retires in ${retirement} years`);
+//       return retirement;
+//     } else {
+//       console.log(`${firstName} has already retired 🎉`);
+//       return -1;
+//     }
+//   }
+  
+//   console.log(yearsUntilRetirement(1991, 'Jonas'));
+//   console.log(yearsUntilRetirement(1950, 'Mike'));
+
+
+
+
+
+
+
+
+
+
+
+
+
