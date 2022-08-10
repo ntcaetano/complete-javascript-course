aad409878ed8279f4bd120c1ad618db589e46211
Natanael Caetano <natacaet@gmail.com>
2022-08-09 21:18:57 -0300

    update

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -339,3 +339,22 @@ HINT: Remember that an array needs a value in each position, and that value can 
 //   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
 // }
 
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+const types = [];
+
+
+for (let i = 0; i < jonas.length; i++) {
+  console.log(jonas[i], typeof jonas[i])
+
+  //types[i] = typeof jonas[i];
+  types.push(typeof jonas[i]);
+}
+
+console.log(types);s
